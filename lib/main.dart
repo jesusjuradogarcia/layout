85772b6af3b1ba890aaf47845e801487f42c0a9d
JESUS_ARNOLDO_JURADO_GARCIA <a.20308051281011@cbtis128.edu.mx>
2023-04-25 17:20:17 +0000

    layo

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,120 @@
 import 'package:flutter/material.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(MyApp());
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Local De INNOVASPORT',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'CdJuarez, Mexico',
+                  style: TextStyle(
+                    color: Color(0xff3497d9),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.add_box,
+            color: Colors.red[500],
+          ),
+          Text('76'),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.access_time, 'Tiempo'),
+          _buildButtonColumn(color, Icons.accessibility, 'ropa'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Este negocio INNOVASPORT es de las mejores tiendas para aquellos'
+        'que les gusta hacer deporte y son fanaticos de la moda'
+        'encuantras los mejores descuentos en ropa deportiva de marca'
+        'accesorios de deportes como balones, bombas de aire, tenis, mochilas'
+        'las mejores marcas como Nike, Adidas, Puma, Underaurmour y muchoa mas ',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'INNOVASPORT',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('INNOVASPORT'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'assets/images/localjrz.jpg',
+              width: 400,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
